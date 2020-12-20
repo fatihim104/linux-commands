3d9dc162df1c50a7d5520d8f680f20af914e7eaa
Fatih imal <fatihimal@gmail.com>
2020-12-20 12:12:24 +0000

    Linux basic shell commands added

---
+++ shell_commands.sh
@@ -0,0 +1,15 @@
+1) pwd
+2) ls -a
+3) mkdir yeniKlasor
+4) touch yeniDosya
+5) cp yeniDosya yeniKlasor
+6) cd yeniKlasor
+7) rm yeniDosya
+8) echo Birinci dosya temel Shell komutlari >> metin.txt
+9) cat metin.txt
+10) grep Shell metin.txt
+11) echo ikinci dosya disk islemleri >> yenimetin.txt
+12) cat metin.txt yenimetin.txt>birlesikDosya.txt
+13) cd ..
+14) rm -r yeniKlasor
+15) cd ..
